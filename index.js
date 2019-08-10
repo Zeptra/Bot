a0134e8e4e1b1a0e27eeaa8c7245418efab9a0d4
ItsBBuck <47763748+ItsBBuck@users.noreply.github.com>
2019-08-09 20:26:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require("discord.js");
+const discord = require("discord.js");
 const bot = new Discord.Client();
 
-const token = "NjA5NTEyNTY0OTk2NzAyMjI1.XU3zgg.IEexN0Y1p380JUp4Cnpsf6he7Ok"
+
 
 bot.on("ready", () => {
   console.log("bot is working");
